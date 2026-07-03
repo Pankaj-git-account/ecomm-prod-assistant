90f134f415616aaf341eae501853b5308f85a9c6
Divyang95 <divyang.thakkar.9231@gmail.com>
2026-07-03 16:28:51 +0530

    infra kubenetes and .github workflows CI Cd pipeline setup folder structure is done

--- prod_assistant/evaluation/ragas_eval.py
+++ prod_assistant/evaluation/ragas_eval.py
@@ -1,11 +1,11 @@
 import asyncio
 from prod_assistant.utils.model_loader import ModelLoader
-# from ragas import SingleTurnSample
-# from ragas.llms import LangchainLLMWrapper
-# from ragas.embeddings import LangchainEmbeddingsWrapper
-# from ragas.metrics import LLMContextPrecisionWithoutReference, ResponseRelevancy
-# import grpc.experimental.aio as grpc_aio
-# grpc_aio.init_grpc_aio()
+from ragas import SingleTurnSample
+from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
+from ragas.metrics import LLMContextPrecisionWithoutReference, ResponseRelevancy
+import grpc.experimental.aio as grpc_aio
+grpc_aio.init_grpc_aio()
 model_loader=ModelLoader()
 
 
